e50b3f1725cd390a797d6a2b60a31fc07512c4ce
jesid acosta pajaro <bikia@mb-pro.local>
2023-02-16 00:06:26 +0100

    chore: add validation dependency

--- src/main/java/tech/proof/ecommerce/ECommercePlatformApplication.java
+++ src/main/java/tech/proof/ecommerce/ECommercePlatformApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ECommercePlatformApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ECommercePlatformApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ECommercePlatformApplication.class, args);
+    }
 
 }
